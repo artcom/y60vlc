8a9d665fa7b46f01f3ba5bb2ccb14a7c6fdfef93
Julian Krumow <julian.krumow@artcom.de>
2013-08-19 17:07:27 +0200

    Removed test.sh

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ENVIRONMENT="../obj/lib/:SCRIPTS:.:/Users/juliank/Development/Y60/y60vlc/src/js:/Users/juliank/Development/Y60/y60vlc_build/lib/"
-
-#y60 -I $ENVIRONMENT tests/testMovieChangeChannels.tst.js rehearsal $*
-y60 -I $ENVIRONMENT tests/testMovieStartStop.tst.js rehearsal $*
-#y60 -I $ENVIRONMENT tests/testMoviePauseResume.tst.js rehearsal $*
-#y60 -I $ENVIRONMENT tests/testMovieLooping.tst.js rehearsal $*
