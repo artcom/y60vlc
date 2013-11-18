d8e5fa994253181f668d57be652c42ca669650be
Mirko Geissler <mirko@sepiolida.local>
2013-11-18 15:56:06 +0100

    Removed temporary files accidentally added. Sorry

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mirko/Development/Samples/y60vlc
-# Build directory: /Users/mirko/Development/Samples/y60vlc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(src)
-SUBDIRS(tests)
